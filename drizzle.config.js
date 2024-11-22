1747f830e16277648918242bc05c4130dce51d4e
jubin2003 <jubinthomas023@gmail.com>
2024-11-22 18:43:19 +0530

    id

--- drizzle.config.js
+++ drizzle.config.js
@@ -4,6 +4,6 @@ export default defineConfig({
   schema: './config/schema.js', // Adjust the path if necessary
   dialect: 'postgresql',
   dbCredentials: {
-    url: 'postgresql://neondb_owner:4KBDTFNoGOs3@ep-crimson-sea-a5muscn8.us-east-2.aws.neon.tech/ai-room-redesign?sslmode=require', // Use environment variable
+    url: ProcessingInstruction.env.NEXT_PUBLIC_DATABASE_URL;
   },
 });
